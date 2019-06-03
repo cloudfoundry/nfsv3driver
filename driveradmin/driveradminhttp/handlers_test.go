271184dccb5ba24b49d9ddbf1ec97889c8ef5337
Dennis Leon <dleon@pivotal.io>
2019-06-03 14:17:25 -0700

    Backfill error edge test cases in handlers_test
    [#166192760] https://www.pivotaltracker.com/story/show/166192760
    
    Signed-off-by: Paul Warren <paul.warren@emc.com>
    Co-authored-by: Paul Warren <paul.warren@emc.com>

--- driveradmin/driveradminhttp/handlers_test.go
+++ driveradmin/driveradminhttp/handlers_test.go
@@ -19,18 +19,18 @@ var _ = Describe("Volman Driver Handlers", func() {
 
 	Context("when generating http handlers", func() {
 		var (
-			testLogger = lagertest.NewTestLogger("HandlersTest")
-			driverAdmin = &nfsdriverfakes.FakeDriverAdmin{}
-			handler http.Handler
-			httpRequest *http.Request
+			testLogger           = lagertest.NewTestLogger("HandlersTest")
+			fakeDriverAdmin      = &nfsdriverfakes.FakeDriverAdmin{}
+			handler              http.Handler
+			httpRequest          *http.Request
 			httpResponseRecorder *httptest.ResponseRecorder
-			route rata.Route
+			route                rata.Route
 
 		)
 
 		BeforeEach(func() {
 			var err error
-			handler, err = driveradminhttp.NewHandler(testLogger, driverAdmin)
+			handler, err = driveradminhttp.NewHandler(testLogger, fakeDriverAdmin)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -46,7 +46,7 @@ var _ = Describe("Volman Driver Handlers", func() {
 
 		Context("Evacuate", func() {
 			BeforeEach(func() {
-				driverAdmin.EvacuateReturns(driveradmin.ErrorResponse{})
+				fakeDriverAdmin.EvacuateReturns(driveradmin.ErrorResponse{})
 
 				var found bool
 				route, found = driveradmin.Routes.FindRouteByName(driveradmin.EvacuateRoute)
@@ -58,22 +58,46 @@ var _ = Describe("Volman Driver Handlers", func() {
 				Expect(httpResponseRecorder.Body).Should(MatchJSON(`{"Err":""}`))
 			})
 
+			Context("when invoking evacuate returns an error", func() {
+				BeforeEach(func() {
+					fakeDriverAdmin.EvacuateReturns(driveradmin.ErrorResponse{
+						Err: "unable to evacuate",
+					})
+				})
+
+				It("should return an http 500 response and an error string", func() {
+					Expect(httpResponseRecorder.Code).To(Equal(500))
+					Expect(httpResponseRecorder.Body).Should(MatchJSON(`{"Err":"unable to evacuate"}`))
+				})
+			})
 		})
 
 		Context("Ping", func() {
 			BeforeEach(func() {
-				driverAdmin.EvacuateReturns(driveradmin.ErrorResponse{})
+				fakeDriverAdmin.EvacuateReturns(driveradmin.ErrorResponse{})
 
 				var found bool
 				route, found = driveradmin.Routes.FindRouteByName(driveradmin.PingRoute)
 				Expect(found).To(BeTrue())
 			})
 
-			It("should produce a handler with an evacuate route", func() {
+			It("should produce a handler with an ping route", func() {
 				Expect(httpResponseRecorder.Code).To(Equal(200))
 				Expect(httpResponseRecorder.Body).Should(MatchJSON(`{"Err":""}`))
 			})
 
+			Context("when invoking ping returns an error", func() {
+				BeforeEach(func() {
+					fakeDriverAdmin.PingReturns(driveradmin.ErrorResponse{
+						Err: "unable to ping",
+					})
+				})
+
+				It("should return an http 500 response and an error string", func() {
+					Expect(httpResponseRecorder.Code).To(Equal(500))
+					Expect(httpResponseRecorder.Body).Should(MatchJSON(`{"Err":"unable to ping"}`))
+				})
+			})
 		})
 
 	})
